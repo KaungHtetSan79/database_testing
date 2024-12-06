650decf6c6d15568da691f817f386b6d0b3f43a5
khs <kk2334261@gmail.com>
2024-12-06 13:31:09 +0630

    First Commit

--- src/main/java/com/testproject/Main.java
+++ src/main/java/com/testproject/Main.java
@@ -3,5 +3,6 @@ package com.testproject;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Every!");
+        System.out.println("Test");
     }
 }
