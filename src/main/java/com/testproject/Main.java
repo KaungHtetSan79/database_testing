d4f5ba8f7c33a10f50cbde1e1f301479cb7b9cba
khs <kk2334261@gmail.com>
2024-12-06 13:48:51 +0630

    Database Connection

--- src/main/java/com/testproject/Main.java
+++ src/main/java/com/testproject/Main.java
@@ -3,6 +3,6 @@ package com.testproject;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Every!");
-        System.out.println("Test");
+        System.out.println("Tes");
     }
 }
